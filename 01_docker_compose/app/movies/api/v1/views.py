78ad1d2f08d0f0f55b0c4f8074e8429a64004d31
Andrey Miskov <amiskov@gmail.com>
2022-04-28 11:10:56 +0300

    Fix code quality issue
    "Давай вынесем одинаковое и большое выражение ArrayAgg в отдельный метод `def _aggregate_person(self, role)` ..."

--- 01_docker_compose/app/movies/api/v1/views.py
+++ 01_docker_compose/app/movies/api/v1/views.py
@@ -11,18 +11,18 @@ class MoviesApiMixin:
     model = Filmwork
     http_method_names = ['get']
 
+    def _aggregate_person(self, role):
+        return ArrayAgg('persons__full_name',
+                        filter=Q(personfilmwork__role__icontains=role),
+                        distinct=True)
+
     def get_queryset(self):
-        return Filmwork.objects.prefetch_related('genres', 'person').values().all() \
+        return Filmwork.objects.prefetch_related('genres', 'person') \
+            .values().all() \
             .annotate(genres=ArrayAgg('genres__name', distinct=True),
-                      actors=ArrayAgg('persons__full_name',
-                                      filter=Q(personfilmwork__role__icontains=Role.ACTOR),
-                                      distinct=True),
-                      directors=ArrayAgg('persons__full_name',
-                                         filter=Q(personfilmwork__role__icontains=Role.DIRECTOR),
-                                         distinct=True),
-                      writers=ArrayAgg('persons__full_name',
-                                       filter=Q(personfilmwork__role__icontains=Role.WRITER),
-                                       distinct=True)
+                      actors=self._aggregate_person(Role.ACTOR),
+                      directors=self._aggregate_person(Role.DIRECTOR),
+                      writers=self._aggregate_person(Role.WRITER),
                       )
 
     def render_to_response(self, context, **response_kwargs):
